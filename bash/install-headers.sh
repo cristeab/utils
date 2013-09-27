9196b447f5624a552421468c43d26df3941697c1
Bogdan Cristea <cristeab@gmail.com>
2013-09-27 10:38:35 +0200

    add install headers

---
+++ bash/install-headers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r qscript/src/script/ qtbase/src/
+cp -r qscript/src/scripttools/ qtbase/src/
+cp -r qtquick1/src/declarative/ qtbase/src/
+cp -r qtmultimedia/src/multimedia/ qtbase/src/
+cp -r qtxmlpatterns/src/xmlpatterns/ qtbase/src/
+
